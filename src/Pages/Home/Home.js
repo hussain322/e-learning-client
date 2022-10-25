4e85f697f969d6ddfd7d8d4cb8cb6dc6624cd949
hussain322 <hussain.ahmed8768@gmail.com>
2022-10-25 21:12:54 +0600

    added banner part

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./Hero/Hero";
+import Slider from "./Slider/Slider";
 
 const Home = () => {
   return (
     <div className="bg-blue-100">
       <Hero />
+      <Slider />
     </div>
   );
 };
